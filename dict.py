fbe95989e0d7397178decce94ac609c4b8f4c84a
Likhitha <likhithaalluri24@gmail.com>
2025-02-04 14:14:20 +0530

    new commit

---
+++ dict.py
@@ -0,0 +1,23 @@
+#1
+# def empty_dict():
+#     empty={}
+#     return empty
+# print(empty_dict())
+#2
+# dictionary1=dict([("name","likhitha"),("age","21"),("city","hyderabad")])
+# print(dictionary1)
+#3
+# sequence = [("name", "Alice"), ("age", 25), ("city", "New York")]
+# dictionary = dict(sequence)
+# print(dictionary)
+#4
+# def dict_values(dict):
+#     for keys in dict.items():
+#         if isnumeric(keys):
+#             return keys
+#     return keys
+# dict=eval(input("enter a dictionary:"))
+# print(dict_values(dict))
+# print(dict.keys())
+# print(dict.values())
+# print(dict.items())
